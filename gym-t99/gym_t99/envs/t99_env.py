6a0127f3cf6826d809ff7f8dfeb7763ed48223cd
Suraj Vathsa <suraj@meetyogi.com>
2021-04-18 22:19:53 -0700

    create helpers for step and basic structure

--- gym-t99/gym_t99/envs/t99_env.py
+++ gym-t99/gym_t99/envs/t99_env.py
@@ -9,6 +9,13 @@ class T99(gym.Env):
     def __init__(self):
         # here we should download saved weights of previous player
         #
+
+        # TODO: Discuss in meeting. 
+        self.action_space = None
+        self.observation_space = None
+        self.state = None
+
+
         print('init t99 2')
 
 
@@ -18,10 +25,17 @@ class T99(gym.Env):
 
         :param dict action:
         :return object observation: full state of the game board.
-        :return float observation: amount of reward achieved by the previous action
+        :return float reward: amount of reward achieved by the previous action
         :return boolean done: whether it’s time to reset the environment again, True if the agent loses or wins
         :return dict info: diagnostic information useful for debugging.
         """
+        reward = self.reward(action)
+        next_state = self.next_state(action)
+        done = self.is_done()
+        info = {}
+        return next_state, reward, done, info
+
+
 
     def reset(self):
         print('reset')
@@ -33,3 +47,21 @@ class T99(gym.Env):
 
     def close(self):
         print('close')
+
+
+    def reward(self, action):
+        """
+        Helper to return reward based on action.
+        """
+        return -1.0
+
+    def next_state(self, action):
+        return None
+
+    def is_done(self):
+        """Helper to return if terminal state is reached.
+        """
+        if self.state:
+            return False
+        else:
+            return True
