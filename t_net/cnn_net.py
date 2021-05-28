41617f9f272cb70a1f11873799a48d89c086d9a2
SlavaInder <v.inderiakin.uk@gmail.com>
2021-05-28 20:08:43 +0000

    alternative FC net

--- t_net/cnn_net.py
+++ t_net/cnn_net.py
@@ -4,32 +4,13 @@ import torch.nn as nn
 class CNNNet(nn.Module):
     def __init__(self):
         super(CNNNet, self).__init__()
-        # input 20 x 10 x 1, output 18 x 8 x 3
-        # self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv1 = nn.Conv2d(1, 3, kernel_size=3)
+        # input 20 x 10 x 1, output 18 x 8 x 3        self.conv1 = nn.Conv2d(1, 3, kernel_size=3)
         # input 18 x 8 x 3, output 14 x 4 x 6
-        # self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
         self.conv2 = nn.Conv2d(3, 6, kernel_size=5)
         # input 14 x 4 x 9, output 11 x 1 x 9
-        # self.conv3 = nn.Conv2d(64, 128, kernel_size=4)
         self.conv3 = nn.Conv2d(6, 9, kernel_size=4)
         # input 99, output 1
-        # self.linear1 = nn.Sequential(nn.Linear(128 * 11, 128))
-        # self.linear2 = nn.Sequential(nn.Linear(128, 1))
         self.linear1 = nn.Sequential(nn.Linear(99, 1))
-        # init mode
-        # self.model = nn.Sequential(
-        #     self.conv1,
-        #     nn.ReLU(),
-        #     self.conv2,
-        #     nn.ReLU(),
-        #     self.conv3,
-        #     nn.ReLU(),
-        #     nn.Flatten(),
-        #     self.linear1
-        #     nn.ReLu(),
-        #     self.linear2
-        # )
         # init mode
         self.model = nn.Sequential(
             self.conv1,
@@ -46,7 +27,7 @@ class CNNNet(nn.Module):
 
     def _create_weights(self):
         for m in self.modules():
-            if isinstance(m, nn.Linear):
+            if isinstance(m, (nn.Linear, nn.Conv2)):
                 nn.init.xavier_uniform_(m.weight)
                 nn.init.constant_(m.bias, 0)
 
--- t_net/fc_net.py
+++ t_net/fc_net.py
@@ -23,3 +23,30 @@ class FCNet(nn.Module):
         x = self.layer3(x)
 
         return x
+
+
+class FCBoardNet(nn.Module):
+    def __init__(self):
+        super(FCNet, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Sequential(nn.Linear(200, 200), nn.ReLU(inplace=True)),
+            nn.ReLU(),
+            nn.Sequential(nn.Linear(200, 200), nn.ReLU(inplace=True)),
+            nn.ReLU(),
+            nn.Sequential(nn.Linear(200, 1), nn.ReLU(inplace=True)),
+        )
+
+        self._create_weights()
+
+    def _create_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        q = self.model(x)
+
+        return q
